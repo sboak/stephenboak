86f2f0258ac768d03fd9e8cb58540ccf6ce1b7df
Stephen Boak <steve@boundary.com>
2011-07-17 21:39:14 -0700

    new projects

--- application.rb
+++ application.rb
@@ -13,3 +13,21 @@ get '/quid' do
   @current_page = 'quid'
   haml :quid, :layout => :'project.layout'
 end
+
+get '/raf' do
+  @title = 'World Bank RAF'
+  @current_page = 'raf'
+  haml :raf, :layout => :'project.layout'
+end
+
+get '/riyadh' do
+  @title = 'Riyadh Region Project Dashboard'
+  @current_page = 'riyadh'
+  haml :riyadh, :layout => :'project.layout'
+end
+
+get '/dragonfly' do
+  @title = 'Dragonfly'
+  @current_page = 'dragonfly'
+  haml :dragonfly, :layout => :'project.layout'
+end
