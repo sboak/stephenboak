6b58e094f52897eedb6fb82be51fb787dea228f9
Stephen Boak <stephen.boak@gmail.com>
2011-07-31 20:02:09 -0700

    tweaks

--- public/js/charts.js
+++ public/js/charts.js
@@ -9,7 +9,7 @@ var current = [
   level: 4,
   description: "Photoshop, Illustrator"},
   {name: "Interaction Design",
-  level: 3,
+  level: 4,
   description: "Omnigraffle, Wireframes, Workflows"},
   {name: "Javascript",
   level: 3,
@@ -40,10 +40,9 @@ var past = [
   description: "G-Code, mills, lathes, routers"}
 ];
 
-var w = 400,
+var w = 450,
     h = 230,
-    offset = 160,
-    x = d3.scale.linear().domain([0, 5]).range([0, w - offset]),
+    x = d3.scale.linear().domain([0, 5]).range([0, w]),
     y = d3.scale.ordinal().domain(d3.range(current.length)).rangeBands([0, h], .2);
     
     var bubbleBox,
@@ -54,44 +53,24 @@ var w = 400,
 // CURRENT CHART //////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 
-var current_vis = d3.select("#current_chart")
-  .append("svg:svg")
-  .attr("width", w)
-  .attr("height", h + 20)
-  .append("svg:g")
-  .attr("transform", "translate(" + offset + ",0)")
-  .on("mouseout", clearBubble());;
+var current_vis = d3.select("#current_chart");
 
-//CREATE GROUPS
-var rules = current_vis.selectAll("g.rule")
-  .data(d3.range(0,5))
-  .enter().append("svg:g")
-  .attr("class", "rules")
-  .attr("transform", function(d) { return "translate(" + x(d) + ",0)"; });
-var bars = current_vis.selectAll("g.bars")
-  .data(current)
-  .enter().append("svg:g")
-  .attr("class", "bars")
-  .attr("transform", function(d, i) { return "translate(" + 0 + "," + y(i) + ")"; });
-var labels = current_vis.selectAll("g.labels")
-  .data(current)
-  .enter().append("svg:g")
-  .attr("class", "labels")
-  .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
-var bubbleGroup= current_vis.append("svg:g").attr("class", "bubble");
-
-rules.append("svg:line")
-  .attr("y1", 0)
-  .attr("y2", h+2)
-  .attr("stroke", "gray");
-
-rules.append("svg:text")
-  .attr("y", h + 9)
-  .attr("dy", ".5em")
+current_vis.selectAll("div.bar").data(current).enter().append("div")
+  .attr("class", "bar current")
+  .style("width", function(d){
+    return x(d.level) + "px";
+  })
   .text(function(d){
-    var x = parseInt(d);
+    return d.name;
+  })
+  .attr("title", function(d){
+    return d.description;
+  });
+  /*
+  .text(function(d){
+    var x = parseInt(d.level);
     if (x<1) {
-      return "None";
+      return "";
     } else if (x>=1 && x<2) {
       return "OK";
     } else if (x>=2 && x<3) {
@@ -101,126 +80,22 @@ rules.append("svg:text")
     } else {
       return "Expert";
     }
-  });
-
-bars.append("svg:rect")
-  .attr("width", function(d){
-    return x(d.level);
-  })
-  .attr("height", y.rangeBand())
-  .on("mouseover", function(d){
-    drawBubble(d.description);
-  })
-  .on("mousemove", moveBubble())
-  .on("mouseout", clearBubble());
-
-labels.append("svg:text")
-  .attr("x", x)
-  .attr("y", y.rangeBand() / 2)
-  .attr("dx", -6)
-  .attr("dy", ".35em")
-  .text(function(d){
-    return d.name;
-  });
-
+  });*/
 
 ///////////////////////////////////////////////////////////
 // PAST CHART /////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 
+var past_vis = d3.select("#past_chart");
 
-h = 90;
-y = d3.scale.ordinal().domain(d3.range(past.length)).rangeBands([0, h], .2);
-
-var past_vis = d3.select("#past_chart")
-  .append("svg:svg")
-  .attr("width", w)
-  .attr("height", h + 20)
-  .append("svg:g")
-  .attr("transform", "translate(" + offset + ",0)");
-
-//CREATE GROUPS
-var rules = past_vis.selectAll("g.rule")
-  .data(d3.range(0,5))
-  .enter().append("svg:g")
-  .attr("class", "rules")
-  .attr("transform", function(d) { return "translate(" + x(d) + ",0)"; });
-var bars = past_vis.selectAll("g.bars")
-  .data(past)
-  .enter().append("svg:g")
-  .attr("class", "bars")
-  .attr("transform", function(d, i) { return "translate(" + 0 + "," + y(i) + ")"; });
-var labels = past_vis.selectAll("g.labels")
-  .data(past)
-  .enter().append("svg:g")
-  .attr("class", "labels")
-  .attr("transform", function(d, i) { return "translate(0," + y(i) + ")"; });
-
-rules.append("svg:line")
-  .attr("y1", 0)
-  .attr("y2", h+2)
-  .attr("stroke", "gray");
-
-rules.append("svg:text")
-  .attr("y", h + 9)
-  .attr("dy", ".5em")
-  .text(function(d){
-    var x = parseInt(d);
-    if (x<1) {
-      return "None";
-    } else if (x>=1 && x<2) {
-      return "OK";
-    } else if (x>=2 && x<3) {
-      return "Good";
-    } else if (x>=3 && x<4) {
-      return "Skilled";
-    } else {
-      return "Expert";
-    }
-  });
-
-bars.append("svg:rect")
-  .attr("width", function(d){
-    return x(d.level);
+past_vis.selectAll("div.bar").data(past).enter().append("div")
+  .attr("class", "bar past")
+  .style("width", function(d){
+    return x(d.level) + "px";
   })
-  .attr("height", y.rangeBand());
-
-labels.append("svg:text")
-  .attr("x", x)
-  .attr("y", y.rangeBand() / 2)
-  .attr("dx", -6)
-  .attr("dy", ".35em")
   .text(function(d){
     return d.name;
+  })
+  .attr("title", function(d){
+    return d.description;
   });
-
-
-///////////////////////////////////////////////////////////
-// FUNCTIONS //////////////////////////////////////////////
-///////////////////////////////////////////////////////////
-
-function drawBubble(description) {
-  clearBubble();
-  bubbleBox = bubbleGroup.append("svg:rect").attr("class", "bubbleBox").attr("width", 200).attr("height", 25);
-  bubbleLabel = bubbleGroup.append("svg:text")
-    .attr("class", "bubbleValue")
-    .attr("dx",5)
-    .attr("dy",18)
-    .attr("height",100)
-    .attr("width", 200)
-    .text(description);
-}
-
-function moveBubble() {
-  return function(d) {
-    bubbleGroup.attr("transform", function() { return "translate(" +[d3.svg.mouse(this.parentNode)[0]+3,d3.svg.mouse(this.parentNode)[1]-27] + ")"; });
-  }
-}
-
-function clearBubble() {
-  if (bubbleGroup) {
-    bubbleGroup.selectAll("rect").remove();
-    bubbleGroup.selectAll("text").remove();
-    bubbleLabel = undefined;
-  }
-}
