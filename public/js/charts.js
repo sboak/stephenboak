509b29000bbe2be9867623b0e579fe8022522faa
Stephen Boak <stephen.boak@gmail.com>
2011-08-02 10:12:05 -0700

    updates

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,7 +1,7 @@
 var current = [
   {name: "HTML",
   level: 4,
-  description: "HTML5, Haml, ERB. I've been writing markup since 1993, and built my first website at  13."},
+  description: "HTML5, Haml, ERB. I've been writing markup since 1993, and built my first website at  14."},
   {name: "CSS",
   level: 4,
   description: "CSS3, Sass, Frameworks (960, blueprint). I've got most properties and design patterns memorized."},
@@ -13,8 +13,8 @@ var current = [
   description: "Omnigraffle, Wireframes, Workflows. I always research and document workflows before building them."},
   {name: "Javascript",
   level: 3,
-  description: "solid jQuery knowledge and DOM manipulation, decent understanding of design patterns in JS."},
-  {name: "Data Viz",
+  description: "Solid understanding of DOM manipulation and jQuery, decent understanding of design patterns in JS."},
+  {name: "Data Visualization",
   level: 3,
   description: "D3, Google Maps API and Charts API, Google Earth, KML. My entire career, in some form, has been about bringing data to life."},
   {name: "Product Management",
@@ -32,17 +32,20 @@ var past = [
   {name: "Parametric Modeling",
   level: 4,
   description: "CATIA, Revit, AutoCAD. Specialized in cost optimization and design-for-manufacturability, I saved almost $5M in the cost of facade production on the Beekman Tower."},
+  {name: "Flash/Actionscript",
+  level: 3,
+  description: "Created the double-vision test above as well as some data visualization projects and a physics engine in grad school."},
   {name: "GIS",
   level: 3,
-  description: "Google Earth, ArcGIS, KML"},
+  description: "Google Earth, ArcGIS, KML. Solid understanding of coordinate systems and data formats, and I could hand-code KML."},
   {name: "CNC Machining",
   level: 2,
-  description: "G-Code, mills, lathes, routers"}
+  description: "I could write g-code and comfortably operate NC and hand-cranked mills, lathes, routers"}
 ];
 
-var w = 450,
+var w = 430,
     h = 230,
-    x = d3.scale.linear().domain([0, 5]).range([0, w]),
+    x = d3.scale.linear().domain([0, 4]).range([0, w]),
     y = d3.scale.ordinal().domain(d3.range(current.length)).rangeBands([0, h], .2);
     
     var bubbleBox,
@@ -60,27 +63,20 @@ current_vis.selectAll("div.bar").data(current).enter().append("div")
   .style("width", function(d){
     return x(d.level) + "px";
   })
-  .text(function(d){
-    return d.name;
+  .html(function(d){
+    var level;
+    if (d.level>3) {
+      level = "Expert";
+    } else if (d.level > 2 && d.level <= 3) {
+      level = "Skilled";
+    }  else if (d.level > 1 && d.level <= 2) {
+      level = "Experienced";
+    }
+    return d.name + '<span class="level">' + level + '</span>';
   })
   .attr("description", function(d){
     return d.description;
   });
-  /*
-  .text(function(d){
-    var x = parseInt(d.level);
-    if (x<1) {
-      return "";
-    } else if (x>=1 && x<2) {
-      return "OK";
-    } else if (x>=2 && x<3) {
-      return "Good";
-    } else if (x>=3 && x<4) {
-      return "Skilled";
-    } else {
-      return "Expert";
-    }
-  });*/
 
 ///////////////////////////////////////////////////////////
 // PAST CHART /////////////////////////////////////////////
@@ -93,8 +89,16 @@ past_vis.selectAll("div.bar").data(past).enter().append("div")
   .style("width", function(d){
     return x(d.level) + "px";
   })
-  .text(function(d){
-    return d.name;
+  .html(function(d){
+    var level;
+    if (d.level>3) {
+      level = "Expert";
+    } else if (d.level > 2 && d.level <= 3) {
+      level = "Skilled";
+    }  else if (d.level > 1 && d.level <= 2) {
+      level = "Experienced";
+    }
+    return d.name + '<span class="level">' + level + '</span>';
   })
   .attr("description", function(d){
     return d.description;
