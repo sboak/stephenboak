63869a27db06c4c572825f514be888aee9f588dc
Stephen Boak <stephen.boak@gmail.com>
2011-08-10 21:41:57 -0700

    added analytics, changed skills, beekman tower name

--- application.rb
+++ application.rb
@@ -41,7 +41,7 @@ get '/' do
     Project.new("Riyadh Region Dashboard","riyadh", "web", "Dashboard for government officials to monitor progress and spending on urban development projects around the Riyadh region.", false),
     Project.new("Dragonfly","dragonfly", "architecture", "Temporary installation at SCIArc inspired by the wing of a dragonfly, hand-made by students. Over 2,000lb of aluminum, attached at only 3 points in the room.", false),
     Project.new("Helios House","helios", "architecture", "Sustainable gas station in Los Angeles featuring solar power, a 100%-recycled steel canopy, rainwater collection, green roof, and LED lighting.", false),
-    Project.new("The Beekman Tower","beekman", "architecture", "Frank Gehry-designed high-rise in NYC featuring a doubly-curved two-layer curtain wall which I optimized for construction.", false),
+    Project.new("New York by Gehry","beekman", "architecture", "Frank Gehry-designed high-rise in NYC featuring a doubly-curved two-layer curtain wall which I optimized for construction.", false),
     Project.new("Diabetic Foot Research","foot", "other", "Worked with the orthotics & prosthetics department to design an insole that significantly reduced ulceration in diabetic patients.", false),
     Project.new("Double-Vision Test","doublevision", "other", "Designed in 2003, partnership with a local ophthalmologist to replace the rudimentary paper test with a digital version. Awarded a provisional patent.", false),
     Project.new("Homemade iPod Case","ipod", "other", "Student project to build an iPod case out of acrylic on a 2.5-axis CNC mill. Wrote all the machine code and made dozens of prototypes.", false)
