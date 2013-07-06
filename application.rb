7b7304217b1fbc6944e821a1b4c7de3d689c0fd0
Stephen Boak <stephen.boak@gmail.com>
2013-07-05 17:36:49 -0700

    v2 big push

--- application.rb
+++ application.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'twitter'
 #require 'haml'
 require File.expand_path(File.dirname(__FILE__) + '/config')
 
@@ -34,65 +35,49 @@ class Project
 end
 
 get '/' do
-  @projects = [
-    Project.new("Boundary", "boundary", "web", "Tools to visualize, explore, and troubleshoot network performance with no hardware to install.", true),
-    Project.new("Quid", "quid", "web", "Web application tracking the performance of private technology companies and allowing users to explore new and emerging companies and spaces.", false),
-    Project.new("World Bank RAF", "raf", "web", "Tool for evaluating energy efficiency opportunities in developing cities by comparing key performance indicators against similar cities.", false),
-    Project.new("Riyadh Region Dashboard","riyadh", "web", "Dashboard for government officials to monitor progress and spending on urban development projects around the Riyadh region.", false),
-    Project.new("Dragonfly","dragonfly", "architecture", "Temporary installation at SCIArc inspired by the wing of a dragonfly, hand-made by students. Over 2,000lb of aluminum, attached at only 3 points in the room.", false),
-    Project.new("Helios House","helios", "architecture", "Sustainable gas station in Los Angeles featuring solar power, a 100%-recycled steel canopy, rainwater collection, green roof, and LED lighting.", false),
-    Project.new("New York by Gehry","beekman", "architecture", "Frank Gehry-designed high-rise in NYC featuring a doubly-curved two-layer curtain wall which I optimized for construction.", false),
-    Project.new("Diabetic Foot Research","foot", "other", "Worked with the orthotics & prosthetics department to design an insole that significantly reduced ulceration in diabetic patients.", false),
-    Project.new("Double-Vision Test","doublevision", "other", "Designed in 2003, partnership with a local ophthalmologist to replace the rudimentary paper test with a digital version. Awarded a provisional patent.", false),
-    Project.new("Homemade iPod Case","ipod", "other", "Student project to build an iPod case out of acrylic on a 2.5-axis CNC mill. Wrote all the machine code and made dozens of prototypes.", false)
-    ]
+  @tweets = Twitter.user_timeline("sboak", :count => 5, :exclude_replies => false)
   @title = 'Home'
   @current_page = 'home'
   erb :index, :layout => :'index.layout'
 end
 
 get '/beekman' do
-  haml :beekman
+  erb :beekman
 end
 
 get '/boundary' do
-  @title = 'Boundary'
-  @current_page = 'boundary'
-  haml :boundary, :layout => :'project.layout'
-end
-
-get '/boundaq' do
-  haml :boundaq
-end
-
-get '/doublevision' do
-  haml :doublevision
+  erb :boundary
 end
 
 get '/dragonfly' do
-  haml :dragonfly
+  erb :dragonfly
 end
-
-get '/foot' do
-  haml :foot
-end
-
 get '/helios' do
-  haml :helios
-end
-
-get '/ipod' do
-  haml :ipod
+  erb :helios
 end
 
 get '/quid' do
-  haml :quid
-end
-
-get '/raf' do
-  haml :raf
+  erb :quid
 end
 
 get '/riyadh' do
-  haml :riyadh
+  erb :riyadh
 end
+
+get '/tankh2o' do
+  erb :tankh2o
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config.rb
+++ config.rb
@@ -1,5 +1,12 @@
 configure do
   # config stuff for you Sinatra app
   set :views, File.dirname(__FILE__) + '/views'
-  set :public, File.dirname(__FILE__) + '/public'
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
+  Twitter.configure do |config|
+    config.consumer_key = "9z2Vz3LRBsmrjqqtQ7k7BA"
+    config.consumer_secret = "DBFnMDxcprx4ieovi9LodBLV0XyQqMACki8folrVDo"
+    config.oauth_token = "7118662-aEPZCS20XizBvS4giCtZhOMska1FOG0ClZEc0NDxGE"
+    config.oauth_token_secret = "kV7wLFihUlufGrgc0mXKYvYK0wqHvJF4zEbzukKo"
+  end
 end
